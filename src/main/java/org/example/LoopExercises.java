b73c7f85a0bffde9a1ac1df872a66652dd50504e
Meera Diraviyam <maildira@gmail.com>
2026-05-03 15:08:00 -0500

    Made necessary changes in conditional and loop exercises

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -2,15 +2,19 @@ package org.example;
 
 public class LoopExercises {
     public int sum(int n) {
-        // Replace the line below with code that returns the sum of the numbers from 1 to n
-        // (use a for loop)
-        return 0;
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            total = total + i;
+        }
+        return total;
     }
 
     public int sumUntilEven(int n) {
-        // Replace the line below with code that returns the sum of the numbers from 1 to n
-        // but stops adding when the sum is even
-        // (use a while loop with a sum variable and a counter variable)
-        return 0;
+        int total = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) break; // Stops when an even number is reached
+            total = total + i;
+        }
+        return total;
     }
 }
